7c7fa8da47037ee0b9e8e70bf6249ea238288ba8
Eugene Roy <box.rz.box@gmail.com>
2023-01-03 20:54:31 +0200

    restart  proj, splits client and server, start server, connected to MongoDB

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose
+  .set("strictQuery", false)
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB connected.");
+    server.listen(PORT, console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
