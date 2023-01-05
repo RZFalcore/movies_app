2a1a5b1aeca7e9d011afdcb550f763446a164b59
Eugene Roy <box.rz.box@gmail.com>
2023-01-05 23:03:13 +0200

    finished media contoller

--- server/controllers/media.controller.js
+++ server/controllers/media.controller.js
@@ -3,6 +3,7 @@ import tmdbApi from "../tmdb/tmdb.api.js";
 import userModel from "../models/user.model.js";
 import favoriteModel from "../models/favorite.model.js";
 import reviewModel from "../models/review.model.js";
+import tokenMiddleware from "../middlewares/token.middleware.js";
 
 const getList = async (req, res) => {
   try {
@@ -32,3 +33,63 @@ const getGenres = async (req, res) => {
     responseHandler.error(res);
   }
 };
+
+const search = async (req, res) => {
+  try {
+    const { mediaType } = req.params;
+    const { query, page } = req.query;
+
+    const searchData = await tmdbApi.mediaSearch({
+      mediaType: "people" ? "person" : mediaType,
+      query,
+      page,
+    });
+
+    return responseHandler.ok(res, searchData);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const getDetails = async (req, res) => {
+  try {
+    const { mediaType, mediaId } = req.params;
+    const params = { mediaType, mediaId };
+
+    const media = await tmdbApi.mediaDetails(params);
+    const credits = await tmdbApi.mediaCredits(params);
+    const videos = await tmdbApi.mediaVideos(params);
+    const recommend = await tmdbApi.mediaRecommend(params);
+    const images = await tmdbApi.mediaImages(params);
+
+    media.credits = credits;
+    media.videos = videos;
+    media.recommend = recommend.results;
+    media.images = images;
+
+    const tokenDecoded = tokenMiddleware.tokenDecode(req);
+
+    if (tokenDecoded) {
+      const user = await userModel.findById(tokenDecoded.data);
+
+      if (user) {
+        const isFavorite = await favoriteModel.findOne({
+          user: user.id,
+          mediaId,
+        });
+        media.isFavorite = isFavorite !== null;
+      }
+    }
+
+    media.reviews = await reviewModel
+      .findOne({ mediaId })
+      .populate("user")
+      .sort("-createdAt");
+
+    responseHandler.ok(res, media);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default { getList, getGenres, search, getDetails };
