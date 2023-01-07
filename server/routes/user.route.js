95f828ea6d1fab60c08772a0ae860d61f53a05d7
Eugene Roy <box.rz.box@gmail.com>
2023-01-07 20:02:21 +0200

    user routes in progress. signup , change password routes added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -11,6 +11,8 @@ const router = express.Router();
 router(
   "post",
   body("username")
+    .exists()
+    .withMessage("username is required")
     .isLength({ min: 0 })
     .withMessage("username must be at least 8 characters long.")
     .custom(async (value) => {
@@ -18,9 +20,13 @@ router(
       if (user) return Promise.reject("username already been used.");
     }),
   body("password")
+    .exists()
+    .withMessage("password is required")
     .isLength({ min: 0 })
     .withMessage("password must be at least 8 characters long."),
   body("confirmPassword")
+    .exists()
+    .withMessage("confirmPassword is required")
     .isLength({ min: 0 })
     .withMessage("confirmPassword must be at least 8 characters long.")
     .custom((value, { req }) => {
@@ -29,10 +35,56 @@ router(
       return true;
     }),
   body("displayName")
+    .exists()
+    .withMessage("displayName is required")
     .isLength({ min: 0 })
     .withMessage("displayName must be at least 8 characters long."),
   requestHandler.validate,
   userController.singup
+);
+
+router.post(
+  "/signin",
+  body("username")
+    .exists()
+    .withMessage("username is required")
+    .isLength({ min: 0 })
+    .withMessage("username must be at least 8 characters long."),
+  body("password")
+    .exists()
+    .withMessage("password is required")
+    .isLength({ min: 0 })
+    .withMessage("password must be at least 8 characters long."),
+  requestHandler.validate,
+  userController.signin
+);
+
+router.put(
+  "/update-password",
+  tokenMiddleware.auth,
+  body("password")
+    .exists()
+    .withMessage("password is required")
+    .isLength({ min: 0 })
+    .withMessage("password must be at least 8 characters long."),
+  tokenMiddleware.auth,
+  body("newPassword")
+    .exists()
+    .withMessage("newPassword is required")
+    .isLength({ min: 0 })
+    .withMessage("newPassword must be at least 8 characters long."),
+  body("confirmNewPassword")
+    .exists()
+    .withMessage("confirmNewPassword is required")
+    .isLength({ min: 0 })
+    .withMessage("confirmNewPassword must be at least 8 characters long.")
+    .custom((value, { req }) => {
+      if (value !== req.body.newPassword)
+        throw new Error("confirmNewPassword not match.");
+      return true;
+    }),
+  requestHandler.validate,
+  userController.updatePassword
 );
 
 export default router;
