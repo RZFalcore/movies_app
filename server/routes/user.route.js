332d5530b025791bd0e643222490d19d5f8118c6
Eugene Roy <box.rz.box@gmail.com>
2023-01-07 23:03:59 +0200

    finished user route, added delete favorite

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -114,4 +114,10 @@ router.post(
   favoriteController.addFavorite
 );
 
+router.delete(
+  "/favorites/:favoriteId",
+  tokenMiddleware.auth,
+  favoriteController.removeFavorite
+);
+
 export default router;
