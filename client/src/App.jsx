9acf5d6967c1dddf8c40f8c44cfef94b5a490a18
Eugene Roy <box.rz.box@gmail.com>
2023-01-15 01:16:46 +0200

    connected themes to app, added toastify config to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@mui/material/styles";
+import themeConfigs from "./configs/theme.configs";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode);
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ themeMode })}>
+      // Toastify configs
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      // MUI reset css
+      <CssBaseline />
+    </ThemeProvider>
   );
 };
 
