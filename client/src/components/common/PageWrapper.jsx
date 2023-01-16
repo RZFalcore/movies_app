a1ac6060cec1203e5ef8fa4958e2925dd34004aa
Eugene Roy <box.rz.box@gmail.com>
2023-01-17 00:37:10 +0200

    pageWrapper added

---
+++ client/src/components/common/PageWrapper.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAppState } from "../../redux/features/appStateSlice.js";
+
+const PageWrapper = ({ state, children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setAppState(state));
+  }, [state]);
+  return children;
+};
+
+export default PageWrapper;
