97a020d493d0b8d413da55dd019803d32631fcaf
Eugene Roy <box.rz.box@gmail.com>
2023-01-17 00:52:21 +0200

    protectedPage wrapper added, app pages files created

--- client/src/components/common/PageWrapper.jsx
+++ client/src/components/common/PageWrapper.jsx
@@ -6,8 +6,11 @@ const PageWrapper = ({ state, children }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    window.scrollTo(0, 0);
+
     dispatch(setAppState(state));
   }, [state]);
+
   return children;
 };
 
