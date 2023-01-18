e0c850a5c8826841a5a0b5dc5267d48db2dc3b9f
Eugene Roy <box.rz.box@gmail.com>
2023-01-18 17:31:24 +0200

    globalLoading finished

--- client/src/components/common/GlobalLoading.jsx
+++ client/src/components/common/GlobalLoading.jsx
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Paper, Box, LinearProgress, Toolbar } from "@mui/material";
 
 const GlobalLoading = () => {
   const { globalLoading } = useSelector((state) => state.globalLoading);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let delay;
+    if (globalLoading) {
+      setIsLoading(true);
+    } else {
+      delay = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(delay);
+    };
+  }, [globalLoading]);
   return (
     <>
-      <Paper></Paper>
+      <Paper
+        sx={{
+          opacity: isLoading ? 1 : 0,
+          pointerEvents: "none",
+          transition: "all .3sec ease",
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 99,
+        }}
+      >
+        <Toolbar />
+        <LinearProgress />
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+          }}
+        ></Box>
+      </Paper>
     </>
   );
 };
