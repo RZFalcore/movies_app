b93d5888af1a343581ec8ea8a87e2c6d73837053
Eugene Roy <box.rz.box@gmail.com>
2023-01-18 17:52:44 +0200

    added container, footer and topbar dummy componensts

--- client/src/components/common/GlobalLoading.jsx
+++ client/src/components/common/GlobalLoading.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Paper, Box, LinearProgress, Toolbar } from "@mui/material";
+import Logo from "./Logo";
 
 const GlobalLoading = () => {
   const { globalLoading } = useSelector((state) => state.globalLoading);
@@ -20,30 +21,30 @@ const GlobalLoading = () => {
     };
   }, [globalLoading]);
   return (
-    <>
-      <Paper
+    <Paper
+      sx={{
+        opacity: isLoading ? 1 : 0,
+        pointerEvents: "none",
+        transition: "all .3sec ease",
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        zIndex: 99,
+      }}
+    >
+      <Toolbar />
+      <LinearProgress />
+      <Box
         sx={{
-          opacity: isLoading ? 1 : 0,
-          pointerEvents: "none",
-          transition: "all .3sec ease",
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-          zIndex: 99,
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
         }}
       >
-        <Toolbar />
-        <LinearProgress />
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%,-50%)",
-          }}
-        ></Box>
-      </Paper>
-    </>
+        <Logo />
+      </Box>
+    </Paper>
   );
 };
 
