1513de64b4df0bad64759bbbbf896723ea2dfe16
Eugene Roy <box.rz.box@gmail.com>
2023-01-18 18:12:14 +0200

    Footer in progress

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Paper, Stack } from "@mui/material";
+import Container from "./Container";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <Container>
+      <Paper square={true} sx={{ backgroundImage: "unset", padding: "2rem" }}>
+        <Stack alignItems="center"></Stack>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Footer;
