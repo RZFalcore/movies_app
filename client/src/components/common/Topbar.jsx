21a6e27d31e6db0628079eea9ba761054adbab30
Eugene Roy <box.rz.box@gmail.com>
2023-01-19 21:08:35 +0200

    Toolbar in progress

--- client/src/components/common/Topbar.jsx
+++ client/src/components/common/Topbar.jsx
@@ -1,4 +1,28 @@
-import React from "react";
+import React, { cloneElement, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import MenuIcon from "@mui/icons-material/Menu";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  useScrollTrigger,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import menuConfigs from "../../configs/menu.configs.js";
+import { themeModes } from "../../configs/theme.configs.js";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice.js";
+import { setThemeMode } from "../../redux/features/themeModeSlice.js";
+import Logo from "./Logo";
+
+const ScrollAppBar = ({ children, window }) => {
+  const { themeMode } = useSelector((state) => state.themeMode);
+  return <></>;
+};
 
 const Topbar = () => {
   return <></>;
