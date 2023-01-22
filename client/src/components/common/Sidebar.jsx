430876728a03a28c67d84517cce2da5e9b30d06f
Eugene Roy <box.rz.box@gmail.com>
2023-01-22 18:34:00 +0200

    sidebar in progress

---
+++ client/src/components/common/Sidebar.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  Drawer,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+
+import Logo from "./Logo";
+import menuConfigs from "../../configs/menu.configs";
+import uiConfigs from "../../configs/ui.configs.js";
+import { themeModes } from "../../configs/theme.configs";
+import { setThemeMode } from "../../redux/features/themeModeSlice";
+
+const Sidebar = ({ open, toggleSidebar }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const { appState } = useSelector((state) => state.appState);
+  const { themeMode } = useSelector((state) => state.themeMode);
+
+  const sideBarWidth = uiConfigs.size.sidebarWidth;
+
+  const onSwitchTheme = () => {
+    const theme =
+      themeMode === themeModes.dark ? themeModes.light : themeModes.dark;
+    dispatch(setThemeMode(theme));
+  };
+
+  const drawer = () => (
+    <>
+      <Toolbar sx={{ paddingY: "20px", color: "text.primary" }}>
+        <Stack width="100%" direction="row" justifyContent="center">
+          <Logo />
+        </Stack>
+      </Toolbar>
+      <List sx={{ paddingX: "30px" }}>
+        <Typography variant="h6" marginBottom="20px" textTransform="uppercase">
+          menu
+        </Typography>
+        {menuConfigs.main.map((item, index) => (
+          <ListItemButton
+            key={index}
+            sx={{
+              borderRadius: "10px",
+              marginY: 1,
+              backgroundColor: appState.includes(item.state)
+                ? "primary.main"
+                : "unset",
+            }}
+            component={Link}
+            to={item.path}
+            onClick={() => toggleSidebar(false)}
+          >
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText
+              disableTypography
+              primary={
+                <Typography textTransform="uppercase">
+                  {item.display}
+                </Typography>
+              }
+            />
+          </ListItemButton>
+        ))}
+        {user && (
+          <>
+            <Typography
+              variant="h6"
+              marginBottom="20px"
+              textTransform="uppercase"
+            >
+              personal
+            </Typography>
+            {menuConfigs.user.map((item, index) => (
+              <ListItemButton
+                key={index}
+                sx={{
+                  borderRadius: "10px",
+                  marginY: 1,
+                  backgroundColor: appState.includes(item.state)
+                    ? "primary.main"
+                    : "unset",
+                }}
+                component={Link}
+                to={item.path}
+                onClick={() => toggleSidebar(false)}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText
+                  disableTypography
+                  primary={
+                    <Typography textTransform="uppercase">
+                      {item.display}
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            ))}
+          </>
+        )}
+
+        <Typography variant="h6" marginBottom="20px" textTransform="uppercase">
+          theme
+        </Typography>
+        <ListItemButton>
+          <ListItemIcon>
+            {themeMode === themeModes.dark ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <WbSunnyOutlinedIcon />
+            )}
+          </ListItemIcon>
+        </ListItemButton>
+      </List>
+    </>
+  );
+  return <>Sidebar</>;
+};
+
+export default Sidebar;
--- client/src/components/common/Topbar.jsx
+++ client/src/components/common/Topbar.jsx
@@ -20,6 +20,7 @@ import { setAuthModalOpen } from "../../redux/features/authModalSlice.js";
 import { setThemeMode } from "../../redux/features/themeModeSlice.js";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu.jsx";
+import Sidebar from "./Sidebar.jsx";
 
 const ScrollAppBar = ({ children, window }) => {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -59,8 +60,12 @@ const Topbar = () => {
       themeMode === themeModes.dark ? themeModes.light : themeModes.dark;
     dispatch(setThemeMode(theme));
   };
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
   return (
     <>
+      <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} />
       <ScrollAppBar>
         <AppBar elevation={0} sx={{ zIndex: 9999 }}>
           <Toolbar
@@ -70,6 +75,7 @@ const Topbar = () => {
               <IconButton
                 color="inherit"
                 sx={{ mr: 2, display: { md: "none" } }}
+                onClick={toggleSidebar}
               >
                 <MenuIcon></MenuIcon>
               </IconButton>
