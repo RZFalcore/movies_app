89148b1781cf589882a7e92fe0778325cec51712
Eugene Roy <box.rz.box@gmail.com>
2023-01-25 00:05:51 +0200

    signin form logic added

--- client/src/components/common/SigninForm.jsx
+++ client/src/components/common/SigninForm.jsx
@@ -11,7 +11,38 @@ import { setUser } from "../../redux/features/userSlice.js";
 
 const SigninForm = ({ switchAuthState }) => {
   const dispatch = useDispatch();
-  const [isLoginRequest, setIsLoginRequest] = useState(false);
+    const [isLoginRequest, setIsLoginRequest] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const signinForm = useFormik({
+      initialValues: {
+        username: "",
+        password: "",
+      },
+      validationSchema: Yup.object({
+        username: Yup.string()
+          .min(8, "User name must be at least 8 characters.")
+          .required("User name is required."),
+        password: Yup.string()
+          .min(8, "Password must be at least 8 characters.")
+          .required("Password is required."),
+      }),
+      onSubmit: async (data) => {
+        setErrorMessage(undefined);
+        setIsLoginRequest(true);
+
+        const { res, err } = await userApi.signin(data);
+        setIsLoginRequest(false);
+
+        if (res) {
+          signinForm.resetForm();
+          dispatch(setUser(res));
+          dispatch(setAuthModalOpen(false));
+          toast.success("Sign in successfully.");
+        }
+
+        if (err) setErrorMessage(err.message);
+      },
+    });
   return <>Sign in</>;
 };
 
