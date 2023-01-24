b90ea32ffb7f98d680343a4f86d8ae08fcbba104
Eugene Roy <box.rz.box@gmail.com>
2023-01-24 23:53:50 +0200

    signinform in progress

--- client/src/components/common/SigninForm.jsx
+++ client/src/components/common/SigninForm.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { LoadingButton } from "@mui/lab";
+import { Alert, Box, Button, Stack, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import userApi from "../../api/modules/user.api.js";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice.js";
+import { setUser } from "../../redux/features/userSlice.js";
 
 const SigninForm = ({ switchAuthState }) => {
+  const dispatch = useDispatch();
+  const [isLoginRequest, setIsLoginRequest] = useState(false);
   return <>Sign in</>;
 };
 
